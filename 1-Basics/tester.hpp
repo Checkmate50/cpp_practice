4b9ae75e76ea8eedfa912cdd4276552400715a12
Dietrich Geisler <dgeisler50@gmail.com>
2016-07-10 16:11:31 -0600

    Removed the test functions from the header file

--- 1-Basics/tester.hpp
+++ 1-Basics/tester.hpp
@@ -12,17 +12,4 @@
 
 void test1();
 
-//private
-int test_mult(int& ntests); //note that ntests returns the number of tests
-int test_is_multiple(int& ntests);
-int test_max(int& ntests);
-int test_sum(int& ntests);
-int test_log_2(int& ntests);
-
-//Called by the above
-bool test(std::string name, int f(int, int), int a, int b, int r); //a-c are parameters, r is the expected result
-bool test(std::string name, bool f(int, int), int a, int b, bool r);
-bool test(std::string name, int f(int, int, int), int a, int b, int c, int r);
-bool test(std::string name, int f(int), int a, int r);
-
 #endif /* BASICS_TESTER_HPP_ */
